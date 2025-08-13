d12daa0de284c4723ffe3a109de308460150016e
予纾 <yushu@srcz.one>
2025-08-13 16:02:24 +0800

    fix: try fix connection

--- src/Program.cs
+++ src/Program.cs
@@ -4,6 +4,11 @@ WebApplicationBuilder builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.AddGrpc(options => options.EnableDetailedErrors = true);
 
+builder.WebHost.ConfigureKestrel(serverOptions =>
+    serverOptions.ListenAnyIP(5000, listenOptions =>
+        listenOptions.Protocols =
+            Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2));
+
 using WebApplication app = builder.Build();
 
 app.MapGrpcService<BestsService>();
