6e2af927d7205b5653085c3355813a4f7dcd998f
予纾 <yushu@srcz.one>
2026-02-12 00:22:22 +0800

    chore: Migrate configuration files from JSON to HOCON format
    - Add the dependency for the HOCON configuration provider package
    - Update Program.cs to load .conf files instead of .json files
    - Remove the original JSON configuration files
    - Add the corresponding HOCON configuration files

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,10 @@
+using Hocon.Extensions.Configuration;
 using Limekuma.Services;
 
 WebApplicationBuilder builder = WebApplication.CreateSlimBuilder(args);
 
+builder.Configuration.AddHoconFile("appsettings.conf", true, true)
+    .AddHoconFile($"appsettings.{builder.Environment.EnvironmentName}.conf", true, true);
 builder.Services.AddGrpc(options => options.EnableDetailedErrors = true);
 
 await using WebApplication app = builder.Build();
